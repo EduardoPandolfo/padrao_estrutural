d795ecf11fc3ec49f5e3d9c7e289d944a2da6608
EduardoPandolfo <ekpandolfo@gmail.com>
2023-08-08 12:44:01 -0300

    exemplo de utilização de outro objeto através de interface

--- src/main/java/br/kp/eduardo/criacional/factorymethod/ref/app/dbadapter/before/Client.java
+++ src/main/java/br/kp/eduardo/criacional/factorymethod/ref/app/dbadapter/before/Client.java
@@ -1,14 +1,30 @@
 package br.kp.eduardo.criacional.factorymethod.ref.app.dbadapter.before;
 
+import br.kp.eduardo.criacional.factorymethod.ref.app.dbadapter.after.db.DB;
 import br.kp.eduardo.criacional.factorymethod.ref.app.dbadapter.after.db.OracleDB;
+import br.kp.eduardo.criacional.factorymethod.ref.app.dbadapter.after.db.PostgresDB;
 
 public class Client {
 
 	public static void main(String[] args) {
-		OracleDB db = new OracleDB();
-		db.query("SELECT * FROM users");
-		db.update("INSERT INTO users VALUES ('User', 25)");
+//		OracleDB db = new OracleDB();
+//		db.query("SELECT * FROM users");
+//		db.update("INSERT INTO users VALUES ('User', 25)");
 		
 		//What happen if you have to change for a postgres database?
+
+		DB dataBase = new OracleDB();
+
+		dataBase.query("select * from users");
+
+		dataBase = new PostgresDB();
+
+		dataBase.query("select * from public.users");
+
+
+		//
+
+
+
 	}
 }
